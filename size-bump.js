0f3da7c2e96142de18c43ecf940a64dd8ec526b8
Brian Zhao <rezha@microsoft.com>
2019-11-22 11:14:07 -0800

    Bundle size limit scenario

---
+++ size-bump.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const bundleSizeLimits = require("./bundle-size-limits.json");
+
+for (const platform in bundleSizeLimits) {
+  const bundleSuffix = (platform === "ios" || platform === "macos") ? "jsbundle": "bundle";
+  const bundleName = `foundation.${platform}.${bundleSuffix}`;
+  const bundleSize = fs.statSync(`./bundles/${platform}/ship/${bundleName}`).size / 1024;
+
+  if (bundleSizeLimits[platform] <= bundleSize) {
+    const newLimit = Math.ceil(bundleSize / 1024);
+    console.log(`Bumping size limit for ${platform} bundle from ${bundleSizeLimits[platform]} KB to ${newLimit} KB`)
+    bundleSizeLimits[platform] = newLimit;
+  }
+}
+
+fs.writeFileSync("./bundle-size-limits.json", JSON.stringify(bundleSizeLimits), {encoding: "utf8"});
+console.log("Bundle size limits adjusted.");
---
+++ size-check.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const bundleSizeLimits = require("./bundle-size-limits.json");
+
+for (const platform in bundleSizeLimits) {
+  const bundleSuffix = (platform === "ios" || platform === "macos") ? "jsbundle": "bundle";
+  const bundleName = `foundation.${platform}.${bundleSuffix}`;
+  const bundleSize = fs.statSync(`./bundles/${platform}/ship/${bundleName}`).size / 1024;
+  const percentage = bundleSize / bundleSizeLimits[platform];
+
+  if (percentage > 1) {
+    console.error(`Bundle size check: ${platform} bundle (${bundleSize.toFixed(2)} KB) exceeds size limit ${bundleSizeLimits[platform]} KB by ${((percentage - 1) * 100).toFixed(2)}%.`);
+  } else {
+    console.log(`Bundle size check: ${platform} bundle (${bundleSize.toFixed(2)} KB) is below size limit ${bundleSizeLimits[platform]} KB.`);
+  }
+}
